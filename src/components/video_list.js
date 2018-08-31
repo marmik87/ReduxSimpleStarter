5ce74f528848346c37181b6403c268819878d84e
Markéta Mikešová <marketa@mikesovi.com>
2018-08-31 09:23:40 +0200

    Add video list and video list detail components

---
+++ src/components/video_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import VideoListDetail from './video_list_detail.js'
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListDetail key={video.etag} video={video} />
+  })
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  )
+}
+
+export default VideoList;
